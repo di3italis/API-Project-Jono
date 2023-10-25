315df5f7d3ebfcee2989d8a1f6fedcab7d76df2b
takashi <takashi.sh@proton.me>
2023-10-25 19:19:05 -0400

    login branch init commit

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,13 +1,12 @@
 const router = require("express").Router();
+const sessionRouter = require("./session.js");
+const usersRouter = require("./users.js");
+const { restoreUser } = require("../../utils/auth.js");
 
 // GET /api/restore-user
-const { restoreUser } = require("../../utils/auth.js");
-router.use(restoreUser);
-
 // router.get("/restore-user", (req, res) => {
 //     return res.json(req.user);
 // });
-
 
 // // GET /api/set-token-cookie
 // const { setTokenCookie } = require("../../utils/auth.js");
@@ -37,8 +36,17 @@ router.use(restoreUser);
 //   }
 // );
 
-// router.post("/test", function (req, res) {
-//     res.json({ requestBody: req.body });
-// });
+// Connect restoreUser middleware to the API router
+// If current user session is valid, set req.user to the user in the database
+// If current user session is not valid, set req.user to null
+router.use(restoreUser);
+
+router.use("/session", sessionRouter);
+
+router.use("/users", usersRouter);
+
+router.post("/test", (req, res) => {
+    res.json({ requestBody: req.body });
+});
 
 module.exports = router;
