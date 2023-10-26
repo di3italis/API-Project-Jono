532ec25ec74ca20acc86bbb2cd3eb727a5e897a1
takashi <takashi.sh@proton.me>
2023-10-26 18:03:18 -0400

    rm console.logs

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,4 +1,4 @@
-console.log("************", process.env.DB_FILE);
+// console.log("************", process.env.DB_FILE);
 module.exports = {
     environment: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 8000,
